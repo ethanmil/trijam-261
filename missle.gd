afc073417c8d2e88573e807e572ab2b2dd7fc4ae
Jory0 <Jorygarrido@gmail.com>
2024-03-18 22:40:53 -0600

    maissiles move towards planet

--- missle.gd
+++ missle.gd
@@ -1,22 +1,22 @@
 extends CharacterBody2D
 
 
-var planet_position = Vector2(0, 0)
+
+var planet_position = Vector2(0,0)
 var speed = 20
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var enemy = get_parent()
-	var main = enemy.get_parent()
-	var planet = main.get_node("planet")
-	
-	planet_position = planet.position
+	var planet = get_node("../../planet")
+	planet_position = planet.global_position
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	velocity = (planet_position - position).normalized() * delta * speed
+	pass
+
+func _physics_process(delta):
+	velocity = (planet_position - global_position).normalized() * delta * speed
 	move_and_collide(velocity)
-	
 	pass
